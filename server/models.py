d83465d1c17d00e0355ebd9eb185814f45a4a2d5
Wesley Peters <wesleypeters@gmail.com>
2023-07-24 15:59:41 -0600

    write code, pass tests

--- server/models.py
+++ server/models.py
@@ -11,7 +11,7 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
+    name = db.Column(db.String)
     birthday = db.Column(db.Date)
     animals = db.relationship('Animal', backref='zookeeper')
 
@@ -19,7 +19,7 @@ class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
-    environment = db.Column(db.String(64))
+    environment = db.Column(db.String)
     open_to_visitors = db.Column(db.Boolean)
     animals = db.relationship('Animal', backref='enclosure')
 
@@ -27,7 +27,7 @@ class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    species = db.Column(db.String(64))
+    name = db.Column(db.String)
+    species = db.Column(db.String)
     zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
     enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
